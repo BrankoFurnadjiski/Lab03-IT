5c291c09fa9a19ed71f7748fbec7f3f9dc8ec20f
Branko <branko.berovomk@outlook.com>
2018-04-07 21:28:24 +0200

    First commit

---
+++ FriendsApp/Controllers/FriendController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FriendsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsApp.Controllers
+{
+    public class FriendController : Controller
+    {
+        private static List<FriendModel> Friends = new List<FriendModel>()
+        {
+            new FriendModel(){Id = 1, Ime = "Marko", MestoZiveenje = "Skopje"},
+            new FriendModel(){Id = 2, Ime = "Darko", MestoZiveenje = "Berovo"},
+            new FriendModel(){Id = 3, Ime = "Zarko", MestoZiveenje = "Bitola"}
+        };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowFriends()
+        {
+            return View(Friends);
+        }
+
+        public ActionResult DeleteFriend(int id)
+        {
+            Friends.RemoveAt(id);
+            return View("ShowFriends", Friends);
+        }
+
+        public ActionResult UpdateFriend(int id)
+        {
+            FriendModel model = Friends.ElementAt(id);
+            model.change = id;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateFriend(FriendModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UpdateFriend", model);
+            }
+            FriendModel updateModel = Friends.ElementAt(model.change);
+            updateModel.Id = model.Id;
+            updateModel.Ime = model.Ime;
+            updateModel.MestoZiveenje = model.MestoZiveenje;
+            return View("ShowFriends", Friends);
+        }
+    }
+}
---
+++ FriendsApp/Models/FriendModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendsApp.Models
+{
+    public class FriendModel
+    {
+        public int change { get; set; }
+
+        [Required(ErrorMessage ="Vnesete id")]
+        [Range(0,200, ErrorMessage = "Mora da e pomegu 0 i 200")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Vnesete ime")]
+        public String Ime { get; set; }
+
+        [Required(ErrorMessage = "Vnesete mesto na ziveenje")]
+        public String MestoZiveenje { get; set; }
+    }
+}
